9f82ab324ab5b8b38d18a918a62054a9f8a1d69b
Ahmedadel200066 <medo.adel200@gmail.com>
2026-03-05 07:49:41 +0200

    as

---
+++ my_dirver_app/lib/mock/fake_auth_service.dart
@@ -0,0 +1,15 @@
+class FakeAuthService {
+
+  Future<Map<String, dynamic>> login(String email, String password) async {
+
+    await Future.delayed(Duration(seconds: 1));
+
+    return {
+      "id": "1",
+      "name": "Test User",
+      "role": "driver"
+    };
+
+  }
+
+}:
